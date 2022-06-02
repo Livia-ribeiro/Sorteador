2f600145db6d2bc039b6c2998547f723cd302607
LIVIA <livianevesribeiro@hotmail.com>
2022-06-02 20:20:52 -0300

    Introdução de  constante e contains.

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
 ﻿//using MinhaBiblioteca;
 //CursoExemplo1.Executar("Lívia");
 #region Variáveis
-int[] numbersPerson1 = new int[3];
-int[] numbersPerson2 = new int[3];
-int[] numbersPerson3 = new int[3];
-int[] numbersPerson4 = new int[3];
-int[] numbersPerson5 = new int[3];
+const int NUMBERS_AMOUNT = 3;
+int[] numbersPerson1 = new int[NUMBERS_AMOUNT];
+int[] numbersPerson2 = new int[NUMBERS_AMOUNT];
+int[] numbersPerson3 = new int[NUMBERS_AMOUNT];
+int[] numbersPerson4 = new int[NUMBERS_AMOUNT];
+int[] numbersPerson5 = new int[NUMBERS_AMOUNT];
 
 bool validPerson1 = false, validPerson2 = false, validPerson3 = false, validPerson4 = false, validPerson5 = false;
 #endregion Variáveis
@@ -19,7 +20,7 @@ int ageFirstPerson = 2022 - birthDate1;
 if (ageFirstPerson >= 18)
 {
     Console.WriteLine("Informe três números de 1 a 10");
-    for(int index = 0; index < 3; index++)
+    for(int index = 0; index < NUMBERS_AMOUNT; index++)
         numbersPerson1[index] = int.Parse(Console.ReadLine());
 
     validPerson1 = true;
@@ -41,7 +42,7 @@ int ageSecondPerson = 2022 - birthDate2;
 if (ageSecondPerson >= 18)
 {
     Console.WriteLine("Informe três números de 1 a 10");
-    for(int index = 0; index < 3; index++)
+    for(int index = 0; index < NUMBERS_AMOUNT; index++)
         numbersPerson2[index] = int.Parse(Console.ReadLine());
   
     validPerson2 = true;
@@ -62,7 +63,7 @@ int ageThirdPerson = 2022 - birthDate3;
 if (ageThirdPerson >= 18)
 {
     Console.WriteLine("Informe três números de 1 a 10");
-    for(int index = 0; index < 3; index++)
+    for(int index = 0; index < NUMBERS_AMOUNT; index++)
         numbersPerson3[index] = int.Parse(Console.ReadLine());
     
     validPerson3 = true;
@@ -83,7 +84,7 @@ int ageFourthPerson = 2022 - birthDate4;
 if (ageFourthPerson >= 18)
 {
     Console.WriteLine("Informe três números de 1 a 10");
-    for(int index = 0; index < 3; index++)
+    for(int index = 0; index < NUMBERS_AMOUNT; index++)
         numbersPerson4[index] = int.Parse(Console.ReadLine());
     
     validPerson4 = true;
@@ -104,7 +105,7 @@ int ageFifthPerson = 2022 - birthDate5;
 if (ageFifthPerson >= 18)
 {
     Console.WriteLine("Informe três números de 1 a 10");
-    for(int index = 0; index < 3; index++)
+    for(int index = 0; index < NUMBERS_AMOUNT; index++)
         numbersPerson5[index] = int.Parse(Console.ReadLine());
     
     validPerson5 = true;
@@ -121,24 +122,21 @@ Random sorteador = new Random();
 int sorteio = 7;  //sorteador.Next(1, 11);
 Console.WriteLine("\nO número sorteado é: " + sorteio);
 
-if (numbersPerson1[0] == sorteio || numbersPerson1[1] == sorteio || numbersPerson1[2] == sorteio || 
-    numbersPerson2[0] == sorteio || numbersPerson2[1] == sorteio || numbersPerson2[2] == sorteio ||
-    numbersPerson3[0] == sorteio || numbersPerson3[1] == sorteio || numbersPerson3[2] == sorteio ||
-    numbersPerson4[0] == sorteio || numbersPerson4[1] == sorteio || numbersPerson4[2] == sorteio ||
-    numbersPerson5[0] == sorteio || numbersPerson5[1] == sorteio || numbersPerson5[2] == sorteio)
+if (numbersPerson1.Contains(sorteio) || numbersPerson2.Contains(sorteio) || numbersPerson3.Contains(sorteio) ||
+    numbersPerson4.Contains(sorteio) || numbersPerson5.Contains(sorteio))
 {
     Console.WriteLine("Alguém ganhou!");
     decimal prizeWinner = 1_000_000;
     int winners = 0;
-    if (numbersPerson1[0] == sorteio || numbersPerson1[1] == sorteio || numbersPerson1[2] == sorteio)
+    if (numbersPerson1.Contains(sorteio))
         winners++;
-    if (numbersPerson2[0] == sorteio || numbersPerson2[1] == sorteio || numbersPerson2[2] == sorteio)
+    if (numbersPerson2.Contains(sorteio))
         winners++;
-    if (numbersPerson3[0] == sorteio || numbersPerson3[1] == sorteio || numbersPerson3[2] == sorteio)
+    if (numbersPerson3.Contains(sorteio))
         winners++;
-    if (numbersPerson4[0] == sorteio || numbersPerson4[1] == sorteio || numbersPerson4[2] == sorteio)
+    if (numbersPerson4.Contains(sorteio))
         winners++;
-    if (numbersPerson5[0] == sorteio || numbersPerson5[1] == sorteio || numbersPerson5[2] == sorteio)
+    if (numbersPerson5.Contains(sorteio))
         winners++;
     decimal divisionWinners = prizeWinner / winners;
     int evenBonus = 0;
@@ -154,15 +152,15 @@ if (numbersPerson1[0] == sorteio || numbersPerson1[1] == sorteio || numbersPerso
     if (sorteio == 7 && winners == 1)
         superBonus = 700_000;
 
-    if (numbersPerson1[0] == sorteio || numbersPerson1[1] == sorteio || numbersPerson1[2] == sorteio)
+    if (numbersPerson1.Contains(sorteio))
         Console.WriteLine($"{firstPerson} de {ageFirstPerson} anos ganhou um prêmio de " + (divisionWinners + prizeAge1 + superBonus));
-    if (numbersPerson2[0] == sorteio || numbersPerson2[1] == sorteio || numbersPerson2[2] == sorteio)
+    if (numbersPerson2.Contains(sorteio))
         Console.WriteLine($"{secondPerson} de {ageSecondPerson} anos ganhou um prêmio de " + (divisionWinners + prizeAge2 + superBonus));
-    if (numbersPerson3[0] == sorteio || numbersPerson3[1] == sorteio || numbersPerson3[2] == sorteio)
+    if (numbersPerson3.Contains(sorteio))
         Console.WriteLine($"{thirdPerson} de {ageThirdPerson} anos ganhou um prêmio de " + (divisionWinners + prizeAge3 + superBonus));
-    if (numbersPerson4[0] == sorteio || numbersPerson4[1] == sorteio || numbersPerson4[2] == sorteio)
+    if (numbersPerson4.Contains(sorteio))
         Console.WriteLine($"{fourthPerson} de {ageFourthPerson} anos ganhou um prêmio de " + (divisionWinners + prizeAge4 + superBonus));
-    if (numbersPerson5[0] == sorteio || numbersPerson5[1] == sorteio || numbersPerson5[2] == sorteio)
+    if (numbersPerson5.Contains(sorteio))
         Console.WriteLine($"{fifthPerson} de {ageFifthPerson} anos ganhou um prêmio de " + (divisionWinners + prizeAge5 + superBonus));
 }
 else
